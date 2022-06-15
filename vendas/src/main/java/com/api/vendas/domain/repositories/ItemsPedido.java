309444ae405f686d349296479e2e079cc06cc9fc
Kayky Freitas <kayky_defreitas@hotmail.com>
2022-06-15 10:28:40 -0300

    Adding search ordering

--- vendas/src/main/java/com/api/vendas/domain/repositories/ItemsPedido.java
+++
@@ -1,7 +0,0 @@
-package com.api.vendas.domain.repositories;
-
-import com.api.vendas.domain.entities.ItemPedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemsPedido extends JpaRepository<ItemPedido, Integer> {
-}
